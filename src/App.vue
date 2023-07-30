e8974242cd3b4c0902ad8c26fd6d77e2ae8a07bd
Web2001zz <1071358158@qq.com>
2023-07-30 11:09:25 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,9 @@
 </script>
 
 <template>
-  <router-link to="/">Go to Home</router-link>
-  <router-link to="/about">Go to About</router-link>
   <router-view></router-view>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+@import '@/assets/css/base.css';
 </style>
